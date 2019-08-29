82188ff27114438bf061f91f0a40ff6a65c1548b
daniel <dev@digitalrepublic.com.br>
2019-08-29 14:40:41 -0300

    Buscando API / Configurando Redux

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,58 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import { MdAddShoppingCart } from "react-icons/md";
+import api from "../../services/api";
 
 import { ProductList } from "./styles";
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-infantil-nike-flex-runner-td/26/HZM-1463-026/HZM-1463-026_detalhe2.jpg?resize=326:*"
-          alt=""
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$129,90</span>
+export default class Home extends Component {
+  state = {
+    products: []
+  };
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+  async componentDidMount() {
+    const response = await api.get("products");
 
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-infantil-nike-flex-runner-td/26/HZM-1463-026/HZM-1463-026_detalhe2.jpg?resize=326:*"
-          alt=""
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$129,90</span>
+    this.setState({ products: response.data });
+  }
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+  render() {
+    const { products } = this.state;
 
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-infantil-nike-flex-runner-td/26/HZM-1463-026/HZM-1463-026_detalhe2.jpg?resize=326:*"
-          alt=""
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$129,90</span>
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>R$ {product.price}</span>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" /> 3
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
